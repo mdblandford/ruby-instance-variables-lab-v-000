4a8f5be5d61a07e0b3717acb6a697be8a9845002
Michael Blandford <mdblandford@gmail.com>
2018-06-22 10:14:49 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,13 @@
 class Dog 
+attr_accessor :name
   
-  def name= (dogs_name)
-    @this_dogs_name = dogs_name
-  end
+  #def name= (dogs_name)
+  #  @this_dogs_name = dogs_name
+  #end
   
-  def name 
-    @this_dogs_name
-  end
+  #def name 
+  #  @this_dogs_name
+  #end
 end 
 
   lassie = Dog.new 
